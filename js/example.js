1f0748074b7b0e196ebe7b060c1fbd884af9665f
aa5835 <aa5835@126.com>
2015-10-10 20:29:30 +0800

    og

---
+++ js/example.js
@@ -0,0 +1,23 @@
+!function ($) {
+  $(function () {
+    // Tooltip and popover demos
+    $('.tooltip-demo').tooltip({
+      selector: '[data-toggle="tooltip"]',
+      container: 'body'
+    });
+
+    $('[data-toggle="popover"]').popover();
+
+    // Demos within modals
+    $('.tooltip-test').tooltip();
+    $('.popover-test').popover();
+
+    // Disable empty links in docs examples
+    $('.bd-example [href=#]').click(function (e) {
+      e.preventDefault();
+    });
+
+
+  });
+}(jQuery);
+
